ec158bdb66be475a783c2d29273c8ed45d7c1a21
Matthew Plough <matt.plough@koboldmetals.com>
2023-08-18 11:21:13 -0400

    Start on packaging Mumps extension
    With this commit, the package gets built but you have to run
    python setup.py bdist twice.  Next up: figure out why.

--- setup.py
+++ setup.py
@@ -5,7 +5,36 @@ pymatsolver is a python package for easy to use matrix solvers.
 
 """
 
-from setuptools import find_packages, setup
+from setuptools import find_packages
+
+# NOTE:
+# numpy.distutils is deprecated because distutils is being removed in Python 3.12.
+# However, using it is the fastest way to get us a working Mumps extension distributed.
+# Once that's in use, we can figure out a long-term distribution solution.
+from numpy.distutils.core import Extension, setup
+
+
+mumps_extension = Extension(
+    name = "pymatsolver.mumps.MumpsInterface",
+    sources = [
+        "pymatsolver/mumps/mumps_interface.f90",
+        "pymatsolver/mumps/mumps_p.f90",
+        "pymatsolver/mumps/mumps_cmplx_p.f90",
+    ],
+    include_dirs=[
+        # this isn't on gfortran's default include path!!
+        "/usr/include",
+    ],
+    libraries=[
+        "zmumps", "cmumps", "dmumps", "smumps", "mumps_common", "mpiseq", "blas", "pord",
+    ],
+    library_dirs=[
+        "/usr/lib"
+    ],
+    extra_f90_compile_args=[
+        "-fcray-pointer", "-fPIC",
+    ],
+)
 
 CLASSIFIERS = [
     'Development Status :: 4 - Beta',
@@ -29,7 +58,7 @@ with open("README.rst") as f:
 setup(
     name="pymatsolver",
     version="0.2.0",
-    packages=find_packages(exclude=["*mumps", "tests"]),
+    packages=find_packages(exclude=["tests"]),
     install_requires=[
         'numpy>=1.7',
         'scipy>=0.13',
@@ -44,5 +73,5 @@ setup(
     download_url="http://github.com/simpeg/pymatsolver",
     classifiers=CLASSIFIERS,
     platforms=["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"],
-    use_2to3=False
+    ext_modules=[mumps_extension],
 )
